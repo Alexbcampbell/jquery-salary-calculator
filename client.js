db4220a9d78ad1667ffcab79a29361602f8e6aea
Alex Campbell <alexbe.campbell@gmail.com>
2020-10-09 16:32:23 -0500

    js class added to input, clickToSubmit function running in js

--- client.js
+++ client.js
@@ -3,5 +3,15 @@ console.log('HELLO');
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('we ready now');
+    $('.js-click-submit').on('click', clickToSubmit);
+    
+}
+
+function clickToSubmit() {
+    const firstName = $('.js-field-firstname').val();
+    const lastName = $('.js-field-lastname').val();
+    const idNumb = $('.js-field-id').val();
+    const jobTitle = $('.js-field-title').val();
+    const annualsalary = $('.js-field-salary').val();
+    console.log(firstName, lastName, idNumb, jobTitle, annualsalary);
 }
